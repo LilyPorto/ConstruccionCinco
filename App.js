1633107100533f87a6a5e0305ec51b7e23f68ecd
Lily Porto <liliana.porto@correo.tdea.edu.co>
2024-04-12 23:07:07 -0500

    Agregando navegacion

--- App.js
+++ App.js
@@ -1,15 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import FormComponent from './src/components/FormComponent';
 import CatalogueComponent from './src/components/CatalogueComponent';
 
+const Tab = createBottomTabNavigator();
+
+
 export default function App() {
   return (
-    <View style={styles.container}>
-      <FormComponent />
-      <CatalogueComponent />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name='catalogue' component={CatalogueComponent} />
+        <Tab.Screen name='form' component={FormComponent} />
+      </Tab.Navigator>
+    </NavigationContainer>
+
   );
 }
 
