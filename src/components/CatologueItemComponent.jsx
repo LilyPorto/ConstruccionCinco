bdaed33f1c1bf696c539182516c7fc5d66385f7a
Lily Porto <liliana.porto@correo.tdea.edu.co>
2024-04-20 19:20:18 -0500

    Agregando las vistas del catalogo

--- src/components/CatologueItemComponent.jsx
+++ src/components/CatologueItemComponent.jsx
@@ -1,8 +1,15 @@
 import React from "react"
-import { View, Text } from "react-native"
+import { View, Text, Image } from "react-native"
 
 const CatalogueItemComponent = (props) => (
-    <View key={props.id} style={{ padding: 20, paddingBottom:5, PaddingTop: 5}}>
+    <View key={props.id} style={{ padding: 20, paddingBottom:5, PaddingTop: 5, justifyContent: "space-around"}}>
+        <Image
+            source={{ uri:props.imagen}}
+            style={{  height: 200 }}
+        />
+        <View style={{ marginBottom:10 }}>
+            <Text style={{ backgroundColor: props.estado == 1 ? '#3339FF':'#FF5233', color:"#fff", padding: 5}}>{ props.estado == 1 ? 'Nuevo':'Usado'}</Text>
+        </View>
         <Text style={{ fontWeight: 'bold', marginBottom: 5 }}>Modelo: {props.modelo}</Text>
         <Text>Marca: {props.marca}</Text>
         <Text>Precie: ${props.precio}</Text>
