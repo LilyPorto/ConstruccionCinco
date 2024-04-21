af023bd5761bc0e8f51023193796bf255976cdf3
LilyPorto <125280436+LilyPorto@users.noreply.github.com>
2024-04-20 19:28:41 -0500

    Feature/agregando vista de taller y navegacion (#14)
    * agregando componente de taller y navegacion en app

--- App.js
+++ App.js
@@ -2,8 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import FormComponent from './src/components/FormComponent';
+import PruebaManejoComponent from './src/components/PruebaManejoComponent';
 import CatalogueComponent from './src/components/CatalogueComponent';
+import CotizacionComponent from './src/components/CotizacionComponent';
+import TallerComponent from './src/components/TallerComponent';
 
 const Tab = createBottomTabNavigator();
 
@@ -12,8 +14,10 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen name='catalogue' component={CatalogueComponent} />
-        <Tab.Screen name='form' component={FormComponent} />
+        <Tab.Screen name='Catalogo' component={CatalogueComponent} />
+        <Tab.Screen name='Prueba de Manejo' component={PruebaManejoComponent} />
+        <Tab.Screen name='Cotizacion de Vehiculo' component={CotizacionComponent} />
+        <Tab.Screen name='Servicio de Taller' component={TallerComponent} />
       </Tab.Navigator>
     </NavigationContainer>
 
